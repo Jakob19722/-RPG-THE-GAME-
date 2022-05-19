9b33983acc79501c444fef46c32834d316a46769
Jakob19722 <96123039+Jakob19722@users.noreply.github.com>
2022-05-19 22:34:20 +0200

    Update draws.py

--- ENDLESS RPG/draws.py
+++ ENDLESS RPG/draws.py
@@ -7,9 +7,18 @@ SCREEN_HEIGHT = 400 + BOTTOM_PANEL
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #800x550 upplösning
 
 
+#npc
+oldwiz_img = pygame.image.load("img/GameObjects/oldwizardman.png").convert_alpha()
+oldwiz_img = pygame.transform.scale(oldwiz_img, (360, 360))
+innkeeper_img = pygame.image.load("img/GameObjects/innkeeper.png").convert_alpha()
+innkeeper_img = pygame.transform.scale(innkeeper_img, (360, 360))
+inndevil_img = pygame.image.load("img/GameObjects/inndevil.png").convert_alpha()
+inndevil_img = pygame.transform.scale(inndevil_img, (360, 360))
 #panel
 pnl_img = pygame.image.load("img/GameObjects/panel.png").convert_alpha()                                        
 
+#gnd
+gnd_img = pygame.image.load("img/GameObjects/ground.png").convert_alpha()
 #intro
 bgintro_img = pygame.image.load("img/Backgrounds/introbg.png").convert_alpha()
 logo_img = pygame.image.load("img/GameObjects/logo.png").convert_alpha()
@@ -17,5 +26,22 @@ press_img = pygame.image.load("img/GameObjects/p2c.png").convert_alpha()
 
 #scene1
 scene_1_img = pygame.image.load("img/Backgrounds/scene1bg.png")
-oldwiz_img = pygame.image.load("img/GameObjects/oldwizardman.png").convert_alpha()
-oldwiz_img = pygame.transform.scale(oldwiz_img, (360, 360))
+
+
+#scene2
+scene_2_img = pygame.image.load("img/Backgrounds/scene2bg.png")
+
+#scene3
+inn_img = pygame.image.load("img/Backgrounds/inn.png")
+
+#scene4
+inn2_img = pygame.image.load("img/Backgrounds/inn2.png")
+
+#scene5
+questboard_img = pygame.image.load("img/Backgrounds/quest.png")
+
+#scene6
+scene_6_img = pygame.image.load("img/Backgrounds/background_3.png")
+
+#bossstage
+bgboss_img = pygame.image.load("img/Backgrounds/background_boss.png")
